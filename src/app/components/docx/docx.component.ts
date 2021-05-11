8d4848db87b01889c649e05704ee45d3afd4e0ca
cloudc.dev@contata.co.in-at-704299081633 <pawanb@contatan.local>
2021-05-11 14:40:41 +0530

    docx update

--- src/app/components/docx/docx.component.ts
+++ src/app/components/docx/docx.component.ts
@@ -16,7 +16,8 @@ export class DocxComponent implements OnInit {
   viewerUrl = "https://docs.google.com/gview?url=%URL%&embedded=true";
   docUrl = {
     doc: "https://drive.google.com/file/d/1DP7T1DOHdj8lA0bC_CdhIkWCrKQY8z7T/view?usp=sharing",
-    docx: "https://drive.google.com/file/d/1ZgzaNwh5USypXOv0X6u3iQJYxdNp8YL7/view?usp=sharing"
+    docx: "https://drive.google.com/file/d/1ZgzaNwh5USypXOv0X6u3iQJYxdNp8YL7/view?usp=sharing",
+    rtf: "https://drive.google.com/file/d/1bmakLZvTRFLByBSuZrlOEidEKWGiH2DH/view?usp=sharing"
   }
 
   constructor() { }
