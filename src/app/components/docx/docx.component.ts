0769b150ab9b1f23e1178a22347f5bc4f29e3e2d
cloudc.dev@contata.co.in-at-704299081633 <pawanb@contatan.local>
2021-05-11 15:25:54 +0530

    s3 url added in docx

--- src/app/components/docx/docx.component.ts
+++ src/app/components/docx/docx.component.ts
@@ -17,7 +17,8 @@ export class DocxComponent implements OnInit {
   docUrl = {
     doc: "https://drive.google.com/file/d/1DP7T1DOHdj8lA0bC_CdhIkWCrKQY8z7T/view?usp=sharing",
     docx: "https://drive.google.com/file/d/1ZgzaNwh5USypXOv0X6u3iQJYxdNp8YL7/view?usp=sharing",
-    rtf: "https://drive.google.com/file/d/1bmakLZvTRFLByBSuZrlOEidEKWGiH2DH/view?usp=sharing"
+    rtf: "https://drive.google.com/file/d/1bmakLZvTRFLByBSuZrlOEidEKWGiH2DH/view?usp=sharing",
+    s3: "https://cloud-court-resources.s3.us-east-2.amazonaws.com/CC+-+High+level+tech+documentation+V1.0.docx"
   }
 
   constructor() { }
