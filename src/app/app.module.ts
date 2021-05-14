819b719acec959d774cb072791e22db740ba0cbd
cloudc.dev@contata.co.in-at-704299081633 <pawanb@contatan.local>
2021-05-14 18:51:03 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,12 @@ import { NgxDocViewerModule } from 'ngx-doc-viewer';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PdfComponent } from './components/pdf/pdf.component';
-import { TxtComponent } from './components/txt/txt.component';
 import { DocxComponent } from './components/docx/docx.component';
-import { PptxComponent } from './components/pptx/pptx.component';
-import { ExcelComponent } from './components/excel/excel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PdfComponent,
-    TxtComponent,
-    DocxComponent,
-    PptxComponent,
-    ExcelComponent
+    DocxComponent
   ],
   imports: [
     BrowserModule,
